e89f2f3ec46570f751f1bff0d0adc7dfb51935e6
javed-hussain <javed.hussain@kcl.ac.uk>
2023-10-23 00:24:17 +0100

    first attempt

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in items:
+        if frequencies[item] > 0:
+            frequencies.update({item:0})
+        else:
+            frequencies[item] = frequencies[item] + 1
+
+
     # Your code goes here
-    return frequencies
+    print(frequencies)
+
+frequencies([2,3,2])
